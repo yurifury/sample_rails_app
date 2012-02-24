2bb74e9239df29d97576a92b9cac38403d77352a
Yuri Feldman <yuri@tbqh.net>
2012-02-24 11:57:35 +0100

    Clean up some User pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def twitterized_type(type)
+    case type
+      when :alert
+        "alert-warning"
+      when :error
+        "alert-error"
+      when :notice
+        "alert-info"
+      when :success
+        "alert-success"
+      else
+        type.to_s
+    end
+  end
 end
